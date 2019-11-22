3b6e173519a79423a9319fb133bad91ffeefc61b
Raylin Aquino <raylinaquino@gmail.com>
2019-11-21 20:33:43 -0400

    Make functionallities + Reviews and Sizes Layout + js interaction and  JSON

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,119 +1,396 @@
-let swiper_breeds_name = '.breeds-container',
-    swiper_breeds_scrollbar = swiper_breeds_name + ' .swiper-scrollbar',
-    swiper_breeds_wrapper = swiper_breeds_name + ' .swiper-wrapper',
-    alphabetLetters = '.sizes-wrap .wrap-alphabet ul li a',
-    activeClass = 'active',
-    sizeSwiper = {};
+;
+(function($) {
 
+    let swiper_breeds_name = '.breeds-container',
+        swiper_prods_name = '.prods-container',
 
-/**
- * Setting the letter by current slide active
- *
- **/
+        swiper_breeds_scrollbar = swiper_breeds_name + ' .swiper-scrollbar',
+        swiper_breeds_wrapper = swiper_breeds_name + ' .swiper-wrapper',
+        alphabetLetters = '.sizes-wrap .wrap-alphabet ul li a',
+        _hideCls = 'hide',
+        _showCls = 'show',
+        _menu =  ' .wrap-menu a',
+        _btnBack = '.btn-back ',
+        activeClass = 'active',
+        _asideBreed = 'aside.breed',
+        _breedProdsSection = '.breed-products',
+        _breedPropsSection = '.breed-properties',
+        _breedSizesSection = '.sizes-section',
+        sizeSwiper = {};
 
-$(alphabetLetters).on('click', function() {
-    
-    var _this = $(this);
-    
-    $(swiper_breeds_wrapper).find('.swiper-slide').each(function() {
-        
-        var breed_val = $(this).find('.breed-name').text()[0];
-        
-        if (breed_val.toLowerCase().trim() === _this.text().toLowerCase().trim()) {
-            sizeSwiper.slideTo($(this).index(),600)
+    /** JSON FILES **/
+    const breedSizesJSON = 'data/breedSizes.json',
+        pwaConfig = 'data/pwa.json',
+        //prodsShopify = 'http://urbanpet.do/p-roducts.json?limit=999';
+        prodsShopify = 'data/prods.json';
+
+    $(function() {
+
+        /**
+         * Setting the letter by current slide active
+         *
+         **/
+
+        $(alphabetLetters).on('click', function() {
+
+            var _this = $(this);
+
+            $(swiper_breeds_wrapper).find('.swiper-slide').each(function() {
+
+                var breed_val = $(this).find('.breed-name').text()[0];
+
+                if (breed_val.toLowerCase().trim() === _this.text().toLowerCase().trim()) {
+                    sizeSwiper.slideTo($(this).index(), 600)
+                    return false;
+                }
+
+            });
+
             return false;
-        }
+        })
 
-    });
 
-    return false;
+ $(_menu).on('click', function(e) {
+ 	e.preventDefault();
+
+
+
+ 	let targetSection = $(this).attr('href'), arrHides = [];
+ 	
+ 	/** Getting the rest of menues for hiding them **/
+ 	$(_menu).each(function(){
+ 		if($(this).attr('href') !== targetSection){
+ 			arrHides.push($(this).attr('href'));
+ 		}
+ 	})
+
+ 	$(_menu).removeClass(activeClass);
+ 	$(this).addClass(activeClass);
+
+ 	showHideSection(targetSection,arrHides );
+
+
 })
+       
 
+        /***
+        When user click on the breed
+        **/
 
-const alphabetChange = (word) => {
+        $(document).on('click', _asideBreed, function(e) {
 
-    $(alphabetLetters).removeClass(activeClass);
+            e.preventDefault();
 
-    $(alphabetLetters).each(function() {
+            let data = {
+                name: $(this).find('.breed-name ').text(),
+                img: $(this).find('img').attr('src'),
+                sizes: $(this).find('.data-sizes').text()
+            };
 
-        if ($(this).text().toLowerCase().trim() == word[0].toLowerCase().trim()) {
+            getBreedData(data);
+            showHideSection(_breedPropsSection, [_breedSizesSection]);
 
-            $(this).addClass(activeClass);
-
-            return false;
-
-        }
-    })
-};
-
-/**
- * Setting the swiper effect for all breeds
- *
- **/
-const swiperBreeds = () => {
-
-    return new Promise((resolve, reject) => {
-        sizeSwiper = new Swiper(swiper_breeds_name, {
-
-            spaceBetween: 10,
-            slidesPerView: 4,
-            speed: 6000,
-            scrollbar: {
-                el: swiper_breeds_scrollbar,
-                //draggable: true,
-            },
-            freeModeMomentumRatio: 1,
-            freeMode: true,
-            freeModeFluid: true,
-            on: {
-                slideChangeTransitionEnd: function() {
-                    alphabetChange($('.swiper-slide-active').find('.breed-name').html())
-
-                },
-            }
         });
-    }); //end promise
-}
 
-/** Load Breed JSON **/
+        $(document).on('click', '.breed-properties .wrap-details .wrap-info ul li a', function(e) {
 
-const loadBreeds = () => {
-
-    let breed_list = '';
-
-    return fetch('data/breedSizes.json')
-        .then(data => data.json())
-        .then(breeds => {
+            e.preventDefault();
+            showHideSection(_breedProdsSection, [_breedPropsSection]);
 
 
-            return new Promise((resolve, reject) => {
-                /** Ordering by name **/
-           
+            loadProds().then(data => {
 
-                Object.entries(breeds).map(val => {
-
-                    breed_list += `
-								<div class="swiper-slide">
-									<aside class="breed trans-3" data-breed="${val[0]}">
-										<div class="breed-icon-wrap trans-3">
-											<img src="${val[1].image}" width="100" alt="dog1">
-										</div>
-										<div class="breed-name text-uppercase">${val[0]}</div>
-									</aside>
-								</div>  
-							`;
+                getProdsByParams('Collars & Harnesses', "Small", data).then(res => {
+                    console.log(res)
+                    swiperProds();
                 });
-
-                $(swiper_breeds_wrapper).html(breed_list);
-
-                swiperBreeds()
-                
-                resolve();
-
-            }); //end promise
+            });
 
 
-        }).catch(console.log)
-}
 
-loadBreeds();
+
+        });
+
+
+
+        /***
+        When user close the breed properties section
+        **/
+        $(_breedPropsSection + ' ' + _btnBack).on('click', function(e) {
+
+            showHideSection(_breedSizesSection, [_breedPropsSection]);
+
+        });
+
+
+        /***
+        When user close the breed products section
+        **/
+        $(_breedProdsSection + ' ' + _btnBack).on('click', function(e) {
+
+            showHideSection(_breedPropsSection, [_breedProdsSection]);
+
+        });
+
+
+
+    })
+
+
+
+    const getProdsByParams = (type, size, data) => {
+
+        return new Promise((resolve, reject) => {
+
+            let products_data = [], liProd = '';
+
+            Object.entries(data.products).map(val => {
+
+                /** First I ask for the type */
+
+                if (val[1].product_type == type) {
+
+                    /** Some products doesn't have variants	
+                    due that... I must double-check if exists variant. */
+
+                    if (val[1].hasOwnProperty('variants')) {
+
+                        val[1].variants.forEach((item) => {
+
+                            if (item.title == size && item.available === true) {
+
+                                /*products_data.push({
+                                    title: val[1].title,
+                                    size: item.title,
+                                    price: calculatePrice(item.price),
+                                    image: (item.featured_image != null) ? item.featured_image.src : '',
+                                });*/
+
+                                const img = (item.featured_image != null) ? item.featured_image.src : '';
+
+                                liProd += `
+									<div class="swiper-slide">
+									<aside class="breed-prod">
+										<div class="img">
+											<img src="${img}" height="120" />
+										</div>
+										<h4 class="title">
+											${val[1].title}
+										</h4>
+										<div class="price">
+											RD$ ${calculatePrice(item.price)}
+										</div>
+									</aside>
+								</div>
+							
+                                    `;
+
+                            }
+
+                        }); //end Variant
+
+                    }
+
+                }
+            }); //end entries
+
+
+            $(swiper_prods_name+' .swiper-wrapper').html(liProd)
+            if (products_data) {
+                return resolve(true);
+            }
+            return reject('no data');
+        });
+
+    }
+
+
+    /**
+     * Change the letter in alphabet depends on the current slide.
+     *
+     **/
+
+    const alphabetChange = (word) => {
+
+        $(alphabetLetters).removeClass(activeClass);
+
+        $(alphabetLetters).each(function() {
+
+            if ($(this).text().toLowerCase().trim() == word[0].toLowerCase().trim()) {
+
+                $(this).addClass(activeClass);
+
+                return false;
+
+            }
+        })
+    };
+
+    const showHideSection = (secToShow, secToHide) => {
+    	 
+     
+    	secToHide.forEach( function(element, index) {
+    		 $(element).removeClass(_showCls).addClass(_hideCls);
+    	});
+
+       
+        $(secToShow).removeClass(_hideCls).addClass(_showCls);
+
+    }
+
+    /**
+     * Setting the swiper effect for all breeds
+     *
+     **/
+    const swiperProds = () => {
+
+        return new Promise((resolve, reject) => {
+            prodsSwiper = new Swiper(swiper_prods_name, {
+                spaceBetween: 10,
+                slidesPerView: 4,
+                speed: 6000, 
+                freeModeMomentumRatio: 1,
+                freeMode: true,
+                freeModeFluid: true
+            });
+        }); //end promise
+    }
+
+    /**
+     * Setting the swiper effect for all breeds
+     *
+     **/
+    const swiperBreeds = () => {
+
+        return new Promise((resolve, reject) => {
+            sizeSwiper = new Swiper(swiper_breeds_name, {
+
+                spaceBetween: 10,
+                slidesPerView: 4,
+                speed: 6000,
+                scrollbar: {
+                    el: swiper_breeds_scrollbar,
+                    //draggable: true,
+                },
+                freeModeMomentumRatio: 1,
+                freeMode: true,
+                freeModeFluid: true,
+                on: {
+                    slideChangeTransitionEnd: function() {
+
+                        alphabetChange($('.swiper-slide-active').find('.breed-name').html())
+
+                    },
+                }
+            });
+        }); //end promise
+    }
+
+
+    const getBreedData = (_breed) => {
+
+        return new Promise((resolve, reject) => {
+
+            let data_breeds = '';
+
+            $(_breedPropsSection + ' .breed_img').attr('src', _breed.img);
+            $(_breedPropsSection + ' h1 span').text(_breed.name);
+            $(_breedPropsSection + ' h1 span').text(_breed.name);
+
+            Object.entries(JSON.parse(_breed.sizes)).map((size) => {
+
+                data_breeds += `
+     			<li>
+	     			<a href="#"  class="trans-3" >
+	     				<div class="txt">${size[0]}</div> <span class="size text-uppercase">${size[1][1]}</span>
+	     			</a>
+     			</li>
+     			`;
+
+            });
+
+            $(_breedPropsSection + ' .wrap-info ul').html(data_breeds)
+
+
+            resolve();
+
+        });
+    }
+
+    const calculatePrice = price => {
+        return (Math.floor(price) * Math.floor(53)).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    };
+
+
+    /** Load Prods JSON **/
+
+    const loadProds = () => {
+
+        return fetch(prodsShopify).then(data => data.json());
+    }
+
+    /** Load PWA Config**/
+    const pwaLoadData = () => {
+
+        return fetch(pwaConfig).then(data => data.json());
+
+    }
+
+
+    /** Load Breed JSON **/
+
+    const loadBreeds = () => {
+
+        let breed_list = '';
+
+        return fetch(breedSizesJSON)
+            .then(data => data.json())
+            .then(breeds => {
+
+
+                return new Promise((resolve, reject) => {
+
+                    Object.entries(breeds).map(val => {
+
+                        const sizes = {
+                            leash: val[1].leash,
+                            collar: val[1].collar,
+                            leash_ruff: val[1].leash_ruff,
+                            obedience: val[1].obedience,
+                            stepin: val[1].stepin,
+                            meshplus: val[1].meshplus,
+                            soft_walk: val[1].soft_walk,
+                            t_shirt: val[1].t_shirt,
+                            sweater: val[1].sweater
+                        };
+
+
+                        breed_list += `
+	     				<div class="swiper-slide">
+		     				<aside class="breed trans-3" data-breed="${val[0]}">
+			     				<div class="breed-icon-wrap trans-3">
+			     					<img src="${val[1].image}" width="100" alt="dog1">
+			     				</div>
+			     				<div class="breed-name text-uppercase">${val[0]}</div>
+			     				<div class="data-sizes hide">${JSON.stringify(sizes)}</div>
+		     				</aside>
+	     				</div>  
+	     				`;
+                    });
+
+                    $(swiper_breeds_wrapper).html(breed_list);
+
+                    resolve();
+
+                }); //end promise
+
+
+            }).catch(console.log)
+    }
+
+
+
+    //console.log(pwaLoadData());
+    loadBreeds().then(() => swiperBreeds());
+
+})(jQuery);
